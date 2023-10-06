338ad1b66cfd81ade31e705d5fff9d6c3a5eb1ce
madhaught <144740749+madhaught@users.noreply.github.com>
2023-10-05 21:01:54 -0400

    finished code, second commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,34 +1,87 @@
-# First need to import the os module to create file paths 
-# and the csv module for reading CSV files
+# import the os module to create file paths and csv module for reading CSV files
 import os
 import csv
 
-# Next create path for reading the CSV file budget_data.csv
+# create path for reading the CSV file budget_data.csv
 csvpath = os.path.join('Resources', 'budget_data.csv')
-
+    
 # open file with path created and create reader with csv
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     # Read and store the header row
     csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
 
-    # Store value of row 2 in csv, to use when determining monthly profit
-    previous = next(csvreader)
-
-    # after the header and row 2, read each row of data and perform functions
+    #define variables and create lists before creating a for loop
+    total_months = 0
+    profits = []
+    profit_change = []
+    dates = []
+   
+   # Use a for loop to: get month count by counting each row of data 
+   # after header row, add each row of data to separate lists
     for row in csvreader:
-        print(row[0])
+        total_months += 1 
+        profits.append(row[1])
+        dates.append(row[0])
+    
+    # define variable and get sum of all data in profits list
+    net_profit = 0
+    for i in profits:
+        i = int(i)
+        net_profit += i
 
-    #calculate values
+        # determine the changes in profit per month by subtracting the profit of
+        # the previous month from the current month (successive difference in values)
+        # https://www.geeksforgeeks.org/python-generate-successive-element-difference-list/
+        profit_change = [int(profits[i + 1]) - int(profits [i]) for i in range (len(profits)-1)]
+    
+    #determine the total change in profits by adding up the individual values
+    # of the change in monthly profits
+    net_change = 0
+    for i in profit_change:
+        i = int(i)
+        net_change += i
+    
+    # create a list of dates with the corresponding length and index numbers
+    # to the profit_change list
+    #https://www.geeksforgeeks.org/python-removing-first-element-of-list/
+    reduced_dates = dates[1:]
 
-    #print analysis
-    #print("Financial Analysis")
-    #print("----------------------------")
-    #print("Total Months: " + total_months)
-    #print("Total: " + net_total)
-    #print("Average Change: " + average_change)
-    #print("Greatest Increase in Profits: " + greatest_profit_increase)
-    #print("Greatest Decrease in Profts: " + greatest_profit_decrease)
-  
+    #determine the average change in profits by dividing net_change by the number
+    # of values in net_change
+    # https://stackoverflow.com/questions/45310254/fixed-digits-after-decimal-with-f-strings
+    average_change = f"{(net_change/(total_months-1)):.2f}"
+   
+    # find the greatest increase in profits and the greatest decrease
+    greatest_increase = max(profit_change)
+    greatest_decrease = min(profit_change)
+
+    # find the index number of the greatest increase and decrease in the list
+    index_increase = profit_change.index(greatest_increase)
+    index_decrease = profit_change.index(greatest_decrease)
+
+    # use the stored index number to find the month of the greatest increase and
+    # decrease in profits in the date list
+    val1 = reduced_dates[index_increase]
+    val2 = reduced_dates[index_decrease]
+
+    # print analysis
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months: " + str(total_months))
+    print("Total: " + "$" + str(net_profit))
+    print("Average Change: " + "$" + average_change)
+    print("Greatest Increase in Profits: " + val1 + " ($" + str(greatest_increase) + ")")
+    print("Greatest Decrease in Profts: " + val2 + " ($" + str(greatest_decrease) + ")")
+
+# write output file
+# https://www.pythontutorial.net/python-basics/python-write-text-file/
+output_path = os.path.join("analysis", "results.txt")
+lines = ["Financial Analysis", "----------------------------", 
+         "Total Months: " + str(total_months), "Total: " + "$" + str(net_profit), 
+         "Average Change: " + "$" + average_change, "Greatest Increase in Profits: " 
+         + val1 + " ($" + str(greatest_increase) + ")", "Greatest Decrease in Profts: " 
+         + val2 + " ($" + str(greatest_decrease) + ")"]
+with open(output_path, 'w') as f:
+    f.writelines('\n'.join(lines))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,7 @@
 import os
 import csv
 
-# create path for reading the CSV file budget_data.csv
+# create path for reading the CSV file election_data.csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 
 # open file with path created and create reader with csv
@@ -11,18 +11,82 @@ with open(csvpath) as csvfile:
 
     # Read and store the header row
     csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
+    
+    #create lists to sort votes into
+    stockham = []
+    degette = []
+    doane = []
 
-    #calculate values
+    # Define variable to count total number of votes and create for loop to count
+    total_votes = 0
+    for row in csvreader:
+        total_votes += 1
+        
+        # sort votes into lists based on which candidate they were cast for
+        if row[2] == "Charles Casper Stockham":
+            stockham.append(row)
+
+        elif row[2] == "Diana DeGette":
+            degette.append(row)
+
+        else:
+            doane.append(row)
+
+    # count the number of votes in each list
+    stockham_votes = 0
+    for i in stockham:
+        stockham_votes +=1
+
+    degette_votes = 0
+    for i in degette:
+        degette_votes += 1
+
+    doane_votes = 0
+    for i in doane:
+        doane_votes += 1
+
+    # Determine the percentage of the total votes each candidate got and 
+    # format it as a percentage
+    # https://stackoverflow.com/questions/5306756/how-to-print-a-percentage-value
+    stockham_percent = f"{(stockham_votes/total_votes):.3%}"
+
+    degette_percent = f"{(degette_votes/total_votes):.3%}"
+
+    doane_percent = f"{(doane_votes/total_votes):.3%}"
+
+    most_votes = max(stockham_percent, degette_percent, doane_percent)
+
+    # determine the winner of the election by which candidate had the most votes
+    winner = "Candidate"
+    if most_votes == stockham_percent:
+        winner = "Charles Casper Stockham"
+
+    elif most_votes == degette_percent:
+        winner = "Diana DeGette"
+
+    else:
+        winner = "Raymon Anthony Doane"
 
     #print analysis
-    #print("Election Results")
-    #print("-------------------------")
-    #print("Total Votes: " + total_votes)
-    #print("-------------------------")
-    #print("Charles Casper Stockham: " + stockham_votes)
-    #print("Diana DeGette: " + degette_votes)
-    #print("Raymon Anthony Doane: " + doane_votes)
-    #print("-------------------------")
-    #print("Winner: " + winner)
-    #print("-------------------------")
+    print("Election Results")
+    print("-------------------------")
+    print("Total Votes: " + str(total_votes))
+    print("-------------------------")
+    print("Charles Casper Stockham: " + stockham_percent + " (" + str(stockham_votes) + ")")
+    print("Diana DeGette: " + degette_percent + " (" + str(degette_votes) + ")")
+    print("Raymon Anthony Doane: " + doane_percent + " (" + str(doane_votes) + ")")
+    print("-------------------------")
+    print("Winner: " + str(winner))
+    print("-------------------------")
+
+# write output file
+# https://www.pythontutorial.net/python-basics/python-write-text-file/
+output_path = os.path.join("analysis", "results.txt")
+lines = ["Election Results", "-------------------------","Total Votes: " 
+         + str(total_votes), "-------------------------", "Charles Casper Stockham: " 
+         + stockham_percent + " (" + str(stockham_votes) + ")", "Diana DeGette: " + 
+         degette_percent + " (" + str(degette_votes) + ")", "Raymon Anthony Doane: " + 
+         doane_percent + " (" + str(doane_votes) + ")", "-------------------------", 
+         "Winner: " + str(winner), "-------------------------"]
+with open(output_path, 'w') as f:
+    f.writelines('\n'.join(lines))
